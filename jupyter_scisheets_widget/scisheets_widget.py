70d8a38f3f886e5907d79592daa60d51d250f154
Alicia Clark <clarka34@uw.edu>
2017-10-09 14:11:02 -0700

    widget now renders and updates properly

--- jupyter_scisheets_widget/scisheets_widget.py
+++ jupyter_scisheets_widget/scisheets_widget.py
@@ -15,35 +15,14 @@ class SciSheetTable(widgets.DOMWidget):
     _model_name = Unicode('SciSheetTableModel').tag(sync=True)
     _view_module = Unicode('jupyter_scisheets_widget').tag(sync=True)
     _model_module = Unicode('jupyter_scisheets_widget').tag(sync=True)
-    _model_data = Unicode('Welcome!').tag(sync=True)
+    _model_data = Unicode().tag(sync=True)
     _model_header = Unicode().tag(sync=True)
-    #_model_row_header = Unicode().tag(sync=True)
+    _model_index = Unicode().tag(sync=True)
 
 
     @default('layout')
     def _default_layout(self):
         return widgets.Layout(height='400px', align_self='stretch')
-
-    def load_df(self, df):
-        if type(df) == pd.core.frame.DataFrame:
-            model_data = df.to_json(orient='split')
-            #import pdb; pdb.set_trace();
-            print(model_data)
-            model_data = ast.literal_eval(model_data)
-            print(model_data)
-            self._model_data = json.dumps(model_data)
-            print(self._model_data)
-            #self._model_data = json.dumps(model_data['data'])
-            #self._model_header = json.dumps(model_data['columns']) 
-            #self._model_row_header = json.dumps(model_data['index']) 
-        else:
-            print('Please enter a pandas dataframe')
-
-    def load_list(self, list_data):
-        if type(list_data) == list:    
-            self._model_data = json.dumps(list_data)
-        else:
-            print('Please enter a list')
 
 
 class HandsonDataFrame(object):
@@ -56,16 +35,11 @@ class HandsonDataFrame(object):
 
     def _on_displayed(self, e):
         if type(self._df) == pd.core.frame.DataFrame:
-            print('on displayed')
             model_data = self._df.to_json(orient='split')
-            print(model_data)
             model_data = ast.literal_eval(model_data)
-            print(model_data)
-            self._widget._model_data = json.dumps(model_data)
-            print(self._widget._model_data)
-            #self._widget._model_data = json.dumps(model_data['data'])
-            #self._widget._model_header = json.dumps(model_data['columns']) 
-            #self._widget._model_row_header = json.dumps(model_data['index']) 
+            self._widget._model_data = json.dumps(model_data['data'])
+            self._widget._model_header = json.dumps(model_data['columns']) 
+            self._widget._model_index = json.dumps(model_data['index']) 
         else:
             print('Please enter a pandas dataframe')
 
@@ -73,16 +47,15 @@ class HandsonDataFrame(object):
         # Widget ==> DataFrame (called every time the user
         # changes a value in the graphical widget)
         print('data is being changed')
-        print(self._widget._model_data)
-        self._widget._model_header = self._widget._model_data    
-        #data_dic = {}
-        #data_dic['columns'] = ast.literal_eval(self._widget._model_header)
-        #data_dic['index'] = ast.literal_eval(self._widget._model_row_header)
-        #data_dic['data'] = ast_literal_eval(self._widget._model_data)
-        #data_dic = ast.literal_eval(self._widget._model_data)
-        #self._df = pd.read_json(json.dumps(data_dic), orient='split')
-        #self._df = pd.read_json(json.dumps(data_dic), orient='split')
-        
+        data_dict = ast.literal_eval(self._widget._model_data)
+        col_dict = ast.literal_eval(self._widget._model_header)
+        index_dict = ast.literal_eval(self._widget._model_index)
+        updated_df = pd.DataFrame(data=data_dict, index=index_dict, 
+                                  columns=col_dict)
+        # Note this will have to be more robust if new rows and/or
+        # columns are added to the widget
+        self._df.update(updated_df)        
+
     def to_dataframe(self):
         return self._df
         
